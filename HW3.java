3f605d46344f1b1e8962afee64cd3cc3a256fe1f
AbeerAlnaamani <111145082+AbeerAlnaamani@users.noreply.github.com>
2022-12-21 23:07:42 +0300

    Add files via upload

---
+++ HW3.java
@@ -0,0 +1,8 @@
+import java.util.Random;
+
+public class HW3 {
+    public static void main(String[] args) {
+        Random r=new Random();
+        System.out.println("random[1,6)"+r.nextInt());
+    }
+}
